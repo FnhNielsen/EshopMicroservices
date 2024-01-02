2265e463781854cf994597aa2d759be7a3f89f7c
oskar566 <oschr20@student.sdu.dk>
2024-01-02 21:30:54 +0100

    added eureka functionality

--- cart_service/src/main/java/dk/sdu/cart_service/CartServiceApplication.java
+++ cart_service/src/main/java/dk/sdu/cart_service/CartServiceApplication.java
@@ -1,17 +1,16 @@
 package dk.sdu.cart_service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 @EnableCaching
 @EnableAsync
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CartServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartServiceApplication.class, args);
--- shipping-service/src/main/java/dk/sdu/shippingservice/ShippingServiceApplication.java
+++ shipping-service/src/main/java/dk/sdu/shippingservice/ShippingServiceApplication.java
@@ -2,8 +2,10 @@ package dk.sdu.shippingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShippingServiceApplication {
 
 	public static void main(String[] args) {
